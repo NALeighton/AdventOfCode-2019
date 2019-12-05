6e418e7747a6e4f88741abad42e2dd3f19b590fe
Nicholas Leighton <naleighton@gmail.com>
2019-12-05 14:25:40 -0500

    Part 1 optimization

--- Day-4/Part-1/Part1.go
+++ Day-4/Part-1/Part1.go
@@ -7,23 +7,6 @@ import (
 	"time"
 )
 
-func checkInc(num int) bool {
-	str := strconv.Itoa(num)
-	chars := strings.Split(str, "")
-	digits := []int{}
-	for _, c := range chars {
-		d, _ := strconv.Atoi(c)
-		digits = append(digits, d)
-	}
-	flag := true
-	for i := 0; i < len(digits)-1; i++ {
-		if digits[i] > digits[i+1] {
-			flag = false
-		}
-	}
-	return flag
-}
-
 func checkDouble(num int) bool {
 	str := strconv.Itoa(num)
 	chars := strings.Split(str, "")
@@ -43,12 +26,23 @@ func checkDouble(num int) bool {
 
 func main() {
 	start := time.Now()
-	bottom := 145888 // Actual input is 145852, but that includes impossible numbers
-	top := 599999    // Actual input is 616942, but that includes impossible numbers
 	count := 0
-	for i := bottom; i <= top; i++ {
-		if checkInc(i) && checkDouble(i) {
-			count++
+	for f := 1; f <= 5; f++ {
+		for e := f; e <= 9; e++ {
+			for d := e; d <= 9; d++ {
+				for c := d; c <= 9; c++ {
+					for b := c; b <= 9; b++ {
+						for a := b; a <= 9; a++ {
+							num := a + (b * 10) + (c * 100) + (d * 1000) + (e * 10000) + (f * 100000)
+							if num >= 145888 {
+								if checkDouble(num) {
+									count++
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 	fmt.Println(count)
